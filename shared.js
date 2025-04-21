0794aceecf1d7ff0adf6eff77793ab8049fb250d
Bam_Kitthinut <110830551+Kitthinut@users.noreply.github.com>
2025-04-21 19:10:22 +0700

    Update shared.js

--- shared.js
+++ shared.js
@@ -1,3 +1,4 @@
+// This is one zone (like "Red zone" from 10m to 12m)
 export class Zone {
   constructor(name, start, end) {
     this.name = name;
@@ -6,6 +7,7 @@ export class Zone {
   }
 }
 
+// This helps manage zones (get, add, delete, reset)
 export class ZoneManager {
   constructor() {
     this.defaultZones = [
@@ -19,24 +21,29 @@ export class ZoneManager {
     this.zones = saved || [...this.defaultZones];
   }
 
+  // Save zones to localStorage
   save() {
     localStorage.setItem("zones", JSON.stringify(this.zones));
   }
 
+  // Get all zones
   getAll() {
     return this.zones;
   }
 
+  // Add a zone
   add(zone) {
     this.zones.push(zone);
     this.save();
   }
 
+  // Delete a zone
   delete(name) {
     this.zones = this.zones.filter((z) => z.name !== name);
     this.save();
   }
 
+  // Reset zones to defaults
   reset() {
     this.zones = [...this.defaultZones];
     this.save();
